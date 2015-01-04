6c4a059ee96efd5e19221ba002aa17ade2e097a1
Tom Willemse <tom@ryuslash.org>
2015-01-04 21:31:42 +0100

    Validate Stamp data

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -12,4 +12,19 @@
 
 class Stamp < ActiveRecord::Base
   attr_accessible :brand, :magazine, :page
+
+  before_save do
+    brand.capitalize!
+    magazine.capitalize!
+  end
+
+  validates :brand, presence: true,
+            length: { maximum: 100 },
+            uniqueness: {
+              scope: [:magazine, :page],
+              message: I18n.t("should only appear on a page once")
+            }
+
+  validates :magazine, presence: true, length: { maximum: 100 }
+  validates :page, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/stamp_spec.rb
+++ spec/models/stamp_spec.rb
@@ -22,4 +22,89 @@ describe Stamp do
   it { should respond_to(:page) }
 
   it { should be_valid }
+
+  describe "When brand is empty" do
+    before { @stamp.brand = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe "When magazine is empty" do
+    before { @stamp.magazine = ' '}
+    it { should_not be_valid }
+  end
+
+  describe "When page is empty" do
+    before { @stamp.page = nil }
+    it { should_not be_valid }
+  end
+
+  describe "When page is not a number" do
+    before { @stamp.page = 'foo' }
+    it { should_not be_valid }
+  end
+
+  describe "When page is less than one" do
+    before { @stamp.page = 0 }
+    it { should_not be_valid }
+  end
+
+  describe "When a brand already appears on a magazine's page" do
+    before do
+      same_stamp = @stamp.dup
+      same_stamp.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "When only the brand changes" do
+    before do
+      same_stamp = @stamp.dup
+      same_stamp.brand = 'Bar'
+      same_stamp.save
+    end
+
+    it { should be_valid }
+  end
+
+  describe "When only the magazine changes" do
+    before do
+      same_stamp = @stamp.dup
+      same_stamp.magazine = 'Some other magazine'
+      same_stamp.save
+    end
+
+    it { should be_valid }
+  end
+
+  describe "When only te page changes"do
+    before do
+      same_stamp = @stamp.dup
+      same_stamp.page = 11
+      same_stamp.save
+    end
+
+    it { should be_valid }
+  end
+
+  describe "When the brand or magazine change case" do
+    before do
+      same_stamp = @stamp.dup
+      same_stamp.magazine = @stamp.magazine.upcase
+      same_stamp.brand = @stamp.brand.upcase
+      same_stamp.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "When the brand is too long" do
+    before { @stamp.brand = 'a' * 101 }
+    it { should_not be_valid }
+  end
+
+  describe "When the magazine name is too long" do
+    before { @stamp.magazine = 'a' * 101 }
+    it { should_not be_valid }
+  end
 end
