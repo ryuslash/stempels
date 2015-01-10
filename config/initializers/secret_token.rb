fc4804cf03d15eec7f9bc7df2de9dd3dc8cc85ab
Tom Willemse <tom@ryuslash.org>
2015-01-10 03:57:30 +0100

    Add secret_token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stempels::Application.config.secret_token = 'e20c1c8e9f69a710de211c7b49f2172065c16b8c24c1085d02978d23d40f1fd44aa5dc5b735b95c03adc1681410cafce63c458b3650774d570007aac709bcff2'
